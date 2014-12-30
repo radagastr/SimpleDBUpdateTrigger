9888dbcaa7f9b6931e599ea813b04a76a93837c4
Emre <Emre@Emre>
2014-12-30 21:17:42 +0200

    Test class v2

--- src/test/test_db.java
+++ src/test/test_db.java
@@ -10,9 +10,11 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 
 import simpledb.remote.SimpleDriver;
-@SuppressWarnings("serial")
+import studentClient.simpledb.SQLInterpreter;
+@SuppressWarnings({ "unused" })
 public class test_db{
 
 	
@@ -25,8 +27,28 @@ private static Connection conn = null;
 			Driver d = new SimpleDriver();
 			conn = d.connect("jdbc:simpledb://localhost", null);
 			
-			doQuery("select sId, sname from student where sId <> 4");
-		
+			Statement stmt = conn.createStatement();
+			String s = "create table TEST5(testýd int, testname varchar(10) )";
+			 stmt.executeUpdate(s);
+			System.out.println("Table TEST5 created.");
+
+			s = "insert into TEST5(testýd, testname) values ";
+			String[] testvals = {"(100, 'test1')",
+								 "(200, 'test2')",
+								 "(300, 'test3')",
+								 "(400, 'test4')"};
+			
+			System.out.println("aaa");
+			 for (int i=0; i<testvals.length; i++)
+			 	stmt.executeUpdate(s + testvals[i]);
+			System.out.println("TEST records inserted.");
+			
+			doUpdate("update TEST5 set testýd=1000 where testýd=100");
+			
+			doQuery("select testýd from TEST5");
+			doUpdate2("DELETE FROM TEST5");
+			 
+					
 		}
 		catch (Exception e) {
 			fail("Connection Fail");
@@ -47,23 +69,59 @@ private static Connection conn = null;
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(cmd);
 			ResultSetMetaData md = rs.getMetaData();
-			if(rs.next()){
-				assertEquals("Query syntax supported", rs.getInt(md.getColumnName(1)),rs.getInt(md.getColumnName(1)));
-			} else {
-				assertEquals("Query syntax supported", rs.getInt(md.getColumnName(1)),rs.getInt(md.getColumnName(1)));
-			}    	
-				rs.close();
-		}
-		catch (SQLException e) {
-			fail("Query Syntax not supported: " + e.getMessage());
+			int numcols = md.getColumnCount();
+			int totalwidth = 0;
+
+			// print header
+			for (int i = 1; i <= numcols; i++) {
+				int width = md.getColumnDisplaySize(i);
+				totalwidth += width;
+				String fmt = "%" + width + "s";
+				System.out.format(fmt, md.getColumnName(i));
+			}
+			System.out.println();
+			for (int i = 0; i < totalwidth; i++)
+				System.out.print("-");
+			System.out.println();
+
+			// print records
+			while (rs.next()) {
+				for (int i = 1; i <= numcols; i++) {
+					String fldname = md.getColumnName(i);
+					int fldtype = md.getColumnType(i);
+					String fmt = "%" + md.getColumnDisplaySize(i);
+					if (fldtype == Types.INTEGER)
+						System.out.format(fmt + "d", rs.getInt(fldname));
+					else
+						System.out.format(fmt + "s", rs.getString(fldname));
+				}
+				System.out.println();
+			}
+			rs.close();
+		} catch (SQLException e) {
+			System.out.println("SQL Exception: " + e.getMessage());
+			e.printStackTrace();
 		}
 	}
+
 
 	private static void doUpdate(String cmd) {
 		try {
 			Statement stmt = conn.createStatement();
 			//int howmany = stmt.executeUpdate(cmd);
-			assertEquals(3,  stmt.executeUpdate(cmd));
+			assertEquals(1,  stmt.executeUpdate(cmd));
+
+			//System.out.println(howmany + " records processed");
+		} catch (SQLException e) {
+			System.out.println("SQL Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	private static void doUpdate2(String cmd) {
+		try {
+			Statement stmt = conn.createStatement();
+			 int howmany = stmt.executeUpdate(cmd);
+			 
 
 			//System.out.println(howmany + " records processed");
 		} catch (SQLException e) {
